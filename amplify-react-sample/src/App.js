f8d750c1788f7a1afa82819e8ad815555f649a2d
McMeekin <tommcm@3c22fb5d6097.ant.amazon.com>
2020-09-26 17:24:57 +1000

    second

--- amplify-react-sample/src/App.js
+++ amplify-react-sample/src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React from 'react';
 
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
@@ -7,113 +6,381 @@ import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createActor } from './graphql/mutations'
 import { listActors } from './graphql/queries'
 
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+
+import { lighten, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import SaveAltIcon from '@material-ui/icons/SaveAlt';
+import FilterListIcon from '@material-ui/icons/FilterList';
 
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+
 
 const Home = () => {
-    const [actors, setActors] = useState([])
-    const [formState, setFormState] = useState({ firstName: '', lastName: '', age: 0, movies: '' })
-
-
-    useEffect(() => {
-        fetchActors()
-
-    }, [])
-
-    const fetchActors = async () => {
-        try {
-            const actorData = await API.graphql(graphqlOperation(listActors))
-            const actors = actorData.data.listActors.items
-            setActors(actors)
-        } catch (err) { console.log('error fetching actors') }
+  function createData(firstName, lastName, movies, age) {
+    return { firstName, lastName, movies, age};
+  }
+  
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
     }
-
-    const setInput = (key, value, isNumber = false) => {
-        value = (isNumber) ? parseInt(value) : value;
-        setFormState({ ...formState, [key]: value })
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
     }
-
-    const addActor = async () => {
-        try {
-            if (!formState.firstName || !formState.lastName) return
-            const actor = { ...formState }
-            setActors([...actors, actor])
-            setFormState({ firstName: '', lastName: '', age: 0, movies: '' })
-            await API.graphql(graphqlOperation(createActor, { input: actor }))
-        } catch (err) {
-            console.log('error creating actor:', err)
-        }
-    }
-
+    return 0;
+  }
+  
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+  
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+  
+  const headCells = [
+    { id: 'FirstName', numeric: false, disablePadding: true, label: 'First Name' },
+    { id: 'LastName', numeric: false, disablePadding: false, label: 'Last Name' },
+    { id: 'Movies', numeric: false, disablePadding: false, label: 'Movies' },
+    { id: 'Age', numeric: true, disablePadding: false, label: 'Age' }
+  ];
+  
+  function EnhancedTableHead(props) {
+    const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+  
     return (
-        <div className="home">
-            <div className="home__table">
-                <TableContainer component={Paper}>
-                    <Table aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell >FirstName</TableCell>
-                                <TableCell >Last Name</TableCell>
-                                <TableCell align="right">Age</TableCell>
-                                <TableCell align="center">Movies</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {actors.map((row) => (
-                                <TableRow key={row?.name}>
-                                    <TableCell component="th" scope="row">
-                                        {row?.firstName}
-                                    </TableCell>
-                                    <TableCell >{row?.lastName}</TableCell>
-                                    <TableCell align="right">{row?.age}</TableCell>
-                                    <TableCell >{row?.movies}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
-            <div className="app__input">
-                <input
-                    onChange={event => setInput('firstName', event.target.value)}
-                    value={formState.firstName}
-                    placeholder="First Name"
-                />
-                <input
-                    onChange={event => setInput('lastName', event.target.value)}
-                    value={formState.lastName}
-                    placeholder="Last Name"
-                />
-                <input
-                    onChange={event => setInput('age', event.target.value, true)}
-                    value={formState.age}
-                    placeholder="Age"
-                />
-                <input
-                    onChange={event => setInput('movies', event.target.value)}
-                    value={formState.movies}
-                    placeholder="Movies"
-                />
-                <button onClick={addActor}>Create Actor</button>
-            </div>
-            <div className="app">
-      <AmplifySignOut />
-    </div>
-        </div>
-        
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox
+              indeterminate={numSelected > 0 && numSelected < rowCount}
+              checked={rowCount > 0 && numSelected === rowCount}
+              onChange={onSelectAllClick}
+              inputProps={{ 'aria-label': 'select all desserts' }}
+            />
+          </TableCell>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? 'right' : 'left'}
+              padding={headCell.disablePadding ? 'none' : 'default'}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <span className={classes.visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </span>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
     );
+  }
+  
+  EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+  };
+  
+  const useToolbarStyles = makeStyles((theme) => ({
+    root: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(1),
+    },
+    highlight:
+      theme.palette.type === 'light'
+        ? {
+            color: theme.palette.secondary.main,
+            backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+          }
+        : {
+            color: theme.palette.text.primary,
+            backgroundColor: theme.palette.secondary.dark,
+          },
+    title: {
+      flex: '1 1 100%',
+    },
+  }));
+  
+  const EnhancedTableToolbar = (props) => {
+    const classes = useToolbarStyles();
+    const { numSelected } = props;
+    const { whichSelected } = props;
+    
+
+    function saveShows() {
+      
+      var json = Object.assign({}, whichSelected);
+      console.log (json);
+
+    };
+  
+    return (
+      <Toolbar
+        className={clsx(classes.root, {
+          [classes.highlight]: numSelected > 0,
+        })}
+      >
+        {numSelected > 0 ? (
+          <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+            {numSelected} selected
+          </Typography>
+        ) : (
+          <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+            Actors
+          </Typography>
+        )}
+  
+        {numSelected > 0 ? (
+          <Tooltip title="Generate">
+            <IconButton aria-label="Generate" onClick={saveShows}>
+              <SaveAltIcon />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title="Filter list">
+            <IconButton aria-label="filter list">
+              <FilterListIcon />
+            </IconButton>
+          </Tooltip>
+        )}
+      </Toolbar>
+    );
+  };
+  
+  EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+  };
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    paper: {
+      width: '100%',
+      marginBottom: theme.spacing(2),
+    },
+    table: {
+      minWidth: 750,
+    },
+    visuallyHidden: {
+      border: 0,
+      clip: 'rect(0 0 0 0)',
+      height: 1,
+      margin: -1,
+      overflow: 'hidden',
+      padding: 0,
+      position: 'absolute',
+      top: 20,
+      width: 1,
+    },
+  }));
+
+  
+  const classes = useStyles();
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const [actors, setActors] = React.useState([])
+  
+  React.useEffect(() => {
+
+    fetchActors()
+
+  }, [])
+
+  const fetchActors = async () => {
+    try {
+        const actorData = await API.graphql(graphqlOperation(listActors))
+        const actors = actorData.data.listActors.items
+        setActors(actors)
+    } catch (err) { console.log('error fetching actors') }
 }
 
-// export default Home;
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = actors.map((n) => n.firstName);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, actors.length - page * rowsPerPage);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <EnhancedTableToolbar numSelected={selected.length} whichSelected={selected} />
+        {/* {console.log(selected)} */}
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+            aria-label="enhanced table"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={actors.length}
+            />
+            <TableBody>
+              {
+                
+                
+                stableSort(actors, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.firstName}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          checked={isItemSelected}
+                          inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                      </TableCell>
+                      <TableCell component="th" id={labelId} scope="row" padding="none">
+                        {row.firstName}
+                      </TableCell>
+                      <TableCell align="left">{row.lastName}</TableCell>
+                      <TableCell align="left">{row.movies}</TableCell>
+                      <TableCell align="left">{row.age}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={actors.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <div className="app">
+      <AmplifySignOut />
+    </div>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </div>
+    
+  );
+}
 
 export default withAuthenticator(Home, true);
-
-
---
+++ amplify-react-sample/src/App_orig.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { createActor } from './graphql/mutations'
+import { listActors } from './graphql/queries'
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+const Home = () => {
+    const [actors, setActors] = useState([])
+    const [formState, setFormState] = useState({ firstName: '', lastName: '', age: 0, movies: '' })
+
+
+    useEffect(() => {
+        fetchActors()
+
+    }, [])
+
+    const fetchActors = async () => {
+        try {
+            const actorData = await API.graphql(graphqlOperation(listActors))
+            const actors = actorData.data.listActors.items
+            setActors(actors)
+        } catch (err) { console.log('error fetching actors') }
+    }
+
+    const setInput = (key, value, isNumber = false) => {
+        value = (isNumber) ? parseInt(value) : value;
+        setFormState({ ...formState, [key]: value })
+    }
+
+    const addActor = async () => {
+        try {
+            if (!formState.firstName || !formState.lastName) return
+            const actor = { ...formState }
+            setActors([...actors, actor])
+            setFormState({ firstName: '', lastName: '', age: 0, movies: '' })
+            await API.graphql(graphqlOperation(createActor, { input: actor }))
+        } catch (err) {
+            console.log('error creating actor:', err)
+        }
+    }
+
+    return (
+        <div className="home">
+            <div className="home__table">
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell >FirstName</TableCell>
+                                <TableCell >Last Name</TableCell>
+                                <TableCell align="right">Age</TableCell>
+                                <TableCell align="center">Movies</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {actors.map((row) => (
+                                <TableRow key={row?.name}>
+                                    <TableCell component="th" scope="row">
+                                        {row?.firstName}
+                                    </TableCell>
+                                    <TableCell >{row?.lastName}</TableCell>
+                                    <TableCell align="right">{row?.age}</TableCell>
+                                    <TableCell >{row?.movies}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+            <div className="app__input">
+                <input
+                    onChange={event => setInput('firstName', event.target.value)}
+                    value={formState.firstName}
+                    placeholder="First Name"
+                />
+                <input
+                    onChange={event => setInput('lastName', event.target.value)}
+                    value={formState.lastName}
+                    placeholder="Last Name"
+                />
+                <input
+                    onChange={event => setInput('age', event.target.value, true)}
+                    value={formState.age}
+                    placeholder="Age"
+                />
+                <input
+                    onChange={event => setInput('movies', event.target.value)}
+                    value={formState.movies}
+                    placeholder="Movies"
+                />
+                <button onClick={addActor}>Create Actor</button>
+            </div>
+            <div className="app">
+      <AmplifySignOut />
+    </div>
+        </div>
+        
+    );
+}
+
+// export default Home;
+
+export default withAuthenticator(Home, true);
+
+
